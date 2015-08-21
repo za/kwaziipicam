72935d42bf01ae388fa835e24b464845870dddc3
za <za@python.or.id>
2015-08-21 16:19:45 +0800

    upload new pictures

--- commit.sh
+++ commit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-HOME=/home/pi/kwaziipicam git add .
-HOME=/home/pi/kwaziipicam git commit -m "upload new pictures"
-HOME=/home/pi/kwaziipicam git push origin master
+cd /home/pi/kwaziipicam
+git add .
+git commit -m "upload new pictures"
+git push origin master
