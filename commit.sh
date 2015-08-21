e4c3cd641ccb3f47cd04087fca9b397587520bc7
za <za@python.or.id>
2015-08-21 16:44:25 +0800

    upload new pictures #bot

--- commit.sh
+++ commit.sh
@@ -3,4 +3,5 @@
 cd /home/pi/kwaziipicam
 git add .
 git commit -m "upload new pictures #bot"
-HOME=/home/pi ssh -Tvvv git@github.com
+# HOME=/home/pi ssh -Tvvv git@github.com
+HOME=/home/pi git push origin master
